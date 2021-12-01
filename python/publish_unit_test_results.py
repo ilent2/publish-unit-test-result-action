1976f653db5db5894c392525a3d6f1f4e071179e
Enrico Minack <github@enrico.minack.dev>
2021-12-01 11:25:38 +0100

    Treat empty input strings as None (#198)

--- python/publish_unit_test_results.py
+++ python/publish_unit_test_results.py
@@ -162,7 +162,8 @@ def get_var(name: str, options: dict) -> Optional[str]:
     Returns the value from the given dict with key 'INPUT_$key',
     or if this does not exist, key 'key'.
     """
-    return options.get(f'INPUT_{name}') or options.get(name)
+    # the last 'or None' turns empty strings into None
+    return options.get(f'INPUT_{name}') or options.get(name) or None
 
 def get_bool(name: str, options: dict, default: bool = False) -> Optional[str]:
     """
